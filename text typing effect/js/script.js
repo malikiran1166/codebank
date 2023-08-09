a34e4c113d1f82e68fb8741966992bb784aa949f
malikiran1166 <141809623+malikiran1166@users.noreply.github.com>
2023-08-09 13:48:35 +0530

    text typing effect

---
+++ text typing effect/js/script.js
@@ -0,0 +1,10 @@
+$("#typed").typed({
+    strings: ["hard", "fun", "a journey", "LIFE"],
+    typeSpeed: 120,
+    startDelay: 0,
+    backSpeed: 60,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: "|",
+    contentType: 'html'
+});
